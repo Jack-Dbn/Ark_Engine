77d351a54750da842daa180d8ab5c093b189bda6
Jack Dobbin <Jack.Dobbin@outlook.com>
2022-10-06 18:07:20 +0100

    Id allocation for entity

--- Ark_GameEngine/src/GameEngine/Entity.cpp
+++ Ark_GameEngine/src/GameEngine/Entity.cpp
@@ -1,1 +1,11 @@
 #include "Entity.h"
+
+unsigned int Entity::m_IdCounter = 0;
+
+Entity::Entity() {
+	m_Id = m_IdCounter++;
+}
+
+unsigned int Entity::GetId() {
+	return m_Id;
+}
--- Ark_GameEngine/src/GameEngine/Entity.h
+++ Ark_GameEngine/src/GameEngine/Entity.h
@@ -1,5 +1,16 @@
 #pragma once
 class Entity
 {
+public:
+	Entity();
+
+	unsigned int GetId();
+
+private:
+	static unsigned int m_IdCounter;
+
+	unsigned int m_Id;
+
+	bool m_ComponentMask[];
 };
 
